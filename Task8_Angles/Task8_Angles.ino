0c985c39de835097b2346cf4c148f56ec8eb7f46
Tyrone <python5668>
2020-10-16 11:10:45 -0600

    Update Task8_Angles.ino

--- Task8_Angles/Task8_Angles.ino
+++ Task8_Angles/Task8_Angles.ino
@@ -1,7 +1,10 @@
 // Task 8 Combining Acceleration and Gyrocope values
 #include<Wire.h>
 const int MPU = 0x68; //Device address
-float gyro_pitch_calibration_value, angle_gyro;
+int gyro_pitch_data_raw, accelerometer_data_raw;
+float angle_gyro, angle_acc;
+long gyro_pitch_calibration_value;
+int acc_calibration_value = 720;
 
 void setup() {
   Serial.begin(9600);
@@ -21,6 +24,17 @@ void setup() {
   Wire.write(0x1B);             //Talk to the ACCEL_CONFIG register (1C hex)
   Wire.write(0x08);             //Set the register bits as 00001000 (+/- 4g full scale range)
   Wire.endTransmission(true);
+  
+  for (int receive_counter = 0; receive_counter < 500; receive_counter++) {     //Create 500 loops
+    if (receive_counter % 15 == 0)digitalWrite(13, !digitalRead(13));       //Change the state of the LED every 15 loops to make the LED blink fast
+    Wire.beginTransmission(MPU);                                            //Start communication with the gyro
+    Wire.write(0x45);                                                       //Gyro Y_OUT
+    Wire.endTransmission();                                                 //End the transmission
+    Wire.requestFrom(MPU, 2);                                               //Request 2 bytes from the gyro
+    gyro_pitch_calibration_value += Wire.read() << 8 | Wire.read();         //Combine the two bytes to make one integer
+    delayMicroseconds(3700);                                                //Wait for 3700 microseconds to simulate the main program loop time, CRITICALL!!
+  }
+  gyro_pitch_calibration_value /= 500;                                      //Divide the total value by 500 to get the avarage gyro offset
 }
 
 void loop() {
@@ -28,31 +42,19 @@ void loop() {
   Wire.write(0x3F);               //Start reading at register 3F
   Wire.endTransmission(true);         //End the transmission
   Wire.requestFrom(MPU, 2);
-  float accelerometer_data_raw = Wire.read() << 8 | Wire.read();            //Combine the two bytes to make one integer
-  accelerometer_data_raw += 150;                                            //Add the accelerometer calibration value
-  float angle_acc = asin((float)accelerometer_data_raw / 8192.0) * 57.296;  //Calculate the current angle according to the accelerometer
-  
-  for (int receive_counter = 0; receive_counter < 500; receive_counter++) { //Create 500 loops
-    if (receive_counter % 15 == 0) digitalWrite(13, !digitalRead(13)); //Change the state of the LED every 15 loops to make the LED blink fast
-    Wire.beginTransmission(MPU); //Start communication with the gyro
-    Wire.write(0x45); //Gyro Y_OUT
-    Wire.endTransmission(true); //End the transmission
-    Wire.requestFrom(MPU, 2); //Request 2 bytes from the gyro
-    gyro_pitch_calibration_value += Wire.read() << 8 | Wire.read(); //Combine the two bytes to make one integer
-    delayMicroseconds(3700);  //Wait for 3700 microseconds to simulate the main program loop time, CRITICAL!!
-  }
-  gyro_pitch_calibration_value /= 500;
+  accelerometer_data_raw = Wire.read() << 8 | Wire.read();            //Combine the two bytes to make one integer
+  accelerometer_data_raw += acc_calibration_value;                    //Add the accelerometer calibration value
+  angle_acc = asin((float)accelerometer_data_raw / 8192.0) * 57.296;  //Calculate the current angle according to the accelerometer
+  Serial.print(angle_acc);
+  Serial.print(" ");
 
   Wire.beginTransmission(MPU);    //Start communication with the gyro
   Wire.write(0x45);               //Start reading at GYRO_YOUT[15:3]
   Wire.endTransmission(true);     //End the transmission
   Wire.requestFrom(MPU, 2);       //Request 2 bytes from the gyro
-
-  float gyro_pitch_data_raw = Wire.read() << 8 | Wire.read();
+  gyro_pitch_data_raw = Wire.read() << 8 | Wire.read();
   gyro_pitch_data_raw -= gyro_pitch_calibration_value;  //Subtract the gyro calibration value
   angle_gyro += gyro_pitch_data_raw * 0.000031;         //Calculate the traveled angle during this loop angle and add this to the angle_gyro variable
-
   angle_gyro = angle_gyro * 0.9996 + angle_acc * 0.0004;      //Correct the drift of the gyro angle with the accelerometer angle
-
   Serial.println(angle_gyro);
 }
